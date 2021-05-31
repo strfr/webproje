2824fe013e35002b99910570e4f2b01ed7f80794
Strfr <aawifoa@gmail.com>
2021-05-31 13:32:39 +0300

    made changes

--- jquery_effects.js
+++ jquery_effects.js
@@ -1,18 +1,17 @@
-var tStart = 0 // Start transition 100px from top
-      , tEnd = 125   // End at 500px
-      , cStart = [68, 66, 60]  // Gold
-      , cEnd = [179, 217, 112] ,
-      c2Start = [7, 175, 253]  // Gold
-      , c2End = [133, 15, 15]  , 
-       cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
-       c2Diff = [c2End[0] - c2Start[0], c2End[1] - c2Start[1], c2End[1] - c2Start[0]];
-    
-        
-    
+    //Background color adjustments
+    var tStart = 0 
+        , tEnd = 125    
+        , cStart = [68, 66, 60]  
+        , cEnd = [26, 82, 37] 
+        , c2Start = [7, 175, 253]  
+        , c2End = [133, 15, 15]  
+        , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
+        c2Diff = [c2End[0] - c2Start[0], c2End[1] - c2Start[1], c2End[1] - c2Start[0]];
+
     $(document).ready(function(){
       $(document).scroll(function() {
-            var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
-            p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
+            var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); 
+            p = Math.min(1, Math.max(0, p)); 
             var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
             var cBg2 = [Math.round(c2Start[0] + c2Diff[0] * p), Math.round(c2Start[1] + c2Diff[1] * p), Math.round(c2Start[2] + c2Diff[2] * p)];
             $("html").css('background-color', 'rgb(' + cBg.join(',') +')');
@@ -26,5 +25,12 @@ var tStart = 0 // Start transition 100px from top
         $("#gallery").fadeIn(1600);
         $("#contact").fadeIn(1800);
         $("#sign").fadeIn(2000);
+
+        $('#ex4').zoom({ on:'click' });
+        $('#ex2').zoom({ on:'click' });
+        $('#ex3').zoom({ on:'click' });
+        $('#ex1').zoom({ on:'click' });
+        
+        
     });
     
